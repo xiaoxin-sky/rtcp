b2408f9b30ae50a26b3aeb97e019214a65228401
xiaoxin-sky <779931346@qq.com>
2024-04-19 18:21:03 +0800

    wip: 请求头解析

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,8 +1,13 @@
-use std::{io::BufRead, pin::Pin};
+use std::{
+    collections::HashMap,
+    io::BufRead,
+    marker::PhantomPinned,
+};
 
 use bytes::BytesMut;
+use nom::{bytes::{complete::tag, streaming::take_while1}, character::is_alphabetic};
 use tokio::{
-    io::{self, AsyncReadExt},
+    io::{self, split, AsyncReadExt, AsyncWriteExt},
     net::{
         tcp::{OwnedReadHalf, OwnedWriteHalf},
         TcpListener, TcpStream,
@@ -20,61 +25,109 @@ async fn main() -> io::Result<()> {
             println!("{ip}");
 
             let mut http_transformer = HttpTransformer::new(tcp_stream);
-            http_transformer.rewrite_host("127.0.0.1".to_string()).await;
+            // http_transformer.rewrite_host("127.0.0.1".to_string()).await;
             http_transformer.run().await;
         });
     }
 }
 
 struct HttpTransformer {
-    host: Option<String>,
-    rd: OwnedReadHalf,
-    wr: OwnedWriteHalf,
-    cursor: usize,
+    headers: HashMap<String, String>,
+    tcp: TcpStream,
+    _marker: PhantomPinned,
 }
 
 impl HttpTransformer {
     pub fn new(tcp_stream: TcpStream) -> Self {
-        let (rd, wr) = tcp_stream.into_split();
         Self {
-            host: None,
-            rd,
-            wr,
-            cursor: 0,
+            headers: HashMap::new(),
+            tcp: tcp_stream,
+            _marker: PhantomPinned,
         }
     }
 
-    pub async fn rewrite_host(&mut self, new_host: String) {
-        self.host = Some(new_host);
+    // pub async fn rewrite_host(&mut self, new_host: String) {
+    //     self.host = Some(new_host);
+    // }
+
+    /// 解析请求头
+    fn parse_header(&mut self, buf: &mut BytesMut) -> io::Result<bool> {
+        let lines = buf.lines();
+        for row in lines.into_iter() {
+            match row {
+                Ok(line) => {
+                    if line.is_empty() {
+                        continue;
+                    }
+
+                    // if line.starts_with(""){}
+                    // 匹配第一个冒号，作为key，剩余的作为 value
+                    let mut a = line.splitn(2, ':');
+                    let key = a.next().unwrap_or("");
+                    let value = a.next().unwrap_or("");
+                    if value.eq("") || key.eq("") {
+                        continue;
+                    }
+                    self.headers
+                        .insert(key.trim().to_string(), value.trim().to_string());
+                    println!("{key}:{value}");
+                }
+                Err(e) => {
+                    eprintln!("❌{e:?}");
+                    return Err(e);
+                }
+            }
+        }
+        println!("{:?}", self.headers);
+        Ok(false)
     }
 
-    fn parse_header(&self, buf: &mut BytesMut) {
-        let _ = buf.lines().map(|item| {
-            let a = item.unwrap();
-            println!("{a}");
-        });
-    }
-
-    pub async fn run(&mut self) {
+    pub async fn run(&mut self) -> io::Result<()> {
         let mut buf = BytesMut::with_capacity(4 * 1024);
 
-        // loop {
-        let n = self.rd.read_buf(&mut buf).await.unwrap();
-        println!("执行{:?}", buf);
-        // self.parse_header(&mut buf);
-        let lines = buf.lines();
-        for a in lines.into_iter() {
-            
-            println!("{:?}", a);
-        }
+        loop {
+            match self.tcp.read_buf(&mut buf).await {
+                Ok(size) => {
+                    // self.parse_header(&mut buf)?;
 
-        // if n == 0 {
-        //     println!("读取完毕{:?}", buf);
-        //     break;
-        // } else {
-        //     self.cursor += n;
-        //     println!("{n}");
-        // }
-        // }
+                    if size == 0 {
+                        println!("✅退出");
+                        break;
+                    }
+
+                    println!("读取大小:{:?}", size);
+                }
+                Err(e) => {
+                    eprintln!("{e:?}");
+                    break;
+                }
+            };
+            // if n == 0 {
+            //     println!("读取完成");
+            //     break;
+            // }
+        }
+        // let a = self.parse_header(&mut buf)?;
+        // println!("{:?}", buf);
+        self.tcp
+            .write_all(b"HTTP/1.1 200 OK\r\nAccess-Control-Allow-Origin: *\r\n\r\nhello world!")
+            .await;
+        self.tcp.flush().await;
+
+        Ok(())
     }
+}
+
+#[test]
+fn prase_request_line() {
+    let line = b"Get /index.html HTTP/1.1\r\n";
+    let method = take_while1(is_alphabetic);
+    let space = take_while1(|c| c == ' ');
+    let url = take_while1(|c| c != ' ');
+    let is_version = |c| c >= b'0' && c <= b'9' || c == b'.';
+    let http = tag("HTTP/");
+    let version = take_while1(is_version);
+    let line_ending = tag("\r\n");
+
+    let method = take_while1(is_alphabetic);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    
+use nom::{
+    bytes::complete::{tag, take_until, take_while1}, error::Error, sequence::{preceded, Tuple}, AsChar
+};
+
+fn main() {}
+
+#[test]
+fn test() {
+    // combine http and version to extract the version string
+    // preceded will return the result of the second parser
+    // if both succeed
+
+    // combine all previous parsers in one function
+    fn request_line(i: &[u8]) {
+        let method = take_while1(AsChar::is_alpha);
+        let space = take_while1(|c| c == b' ');
+        let space1 = take_while1(|c| c == b' ');
+        let url = take_while1(|c| c != b' ');
+        let is_version = |c| c >= b'0' && c <= b'9' || c == b'.';
+        let http = take_until::<&str, &[u8], Error<&[u8]>>("HTTP/");
+        let version = take_while1(is_version);
+        let line_ending = tag("\r\n");
+        let http_version = preceded(http, version);
+        // Tuples of parsers are a parser themselves,
+        // parsing with each of them sequentially and returning a tuple of their results.
+        // Unlike most other parsers, parser tuples are not `FnMut`, they must be wrapped
+        // in the `parse` function to be able to be used in the same way as the others.
+        let a = (method, space, url, space1, http_version, line_ending).parse(i);
+        println!("{a:?}");
+
+
+        // Ok((input, (method, url, version)))
+    }
+
+    request_line(b"Get /index.html Http/1.1\r\n");
 }
