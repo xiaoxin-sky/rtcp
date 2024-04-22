dfc58bd9dd46b9311286403268d3733955857635
xiaoxin-sky <779931346@qq.com>
2024-04-22 18:27:03 +0800

    wip: add server listen

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,27 +1,35 @@
-use std::{collections::HashMap, io::Error, marker::PhantomPinned};
+use std::{collections::HashMap, io::Error, marker::PhantomPinned, sync::Arc};
 
 use bytes::{Buf, BufMut, Bytes, BytesMut};
 use headers::{Header, HeaderMapExt};
-use rtcp::parser::{parser_request_head_all, RequestLine};
+use rtcp::{
+    parser::{parser_request_head_all, RequestLine},
+    protocol::{RTCPMessage, RTCPType},
+};
 use tokio::{
     io::{self, AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpStream},
+    sync::Mutex,
 };
 
 /// 创建通道服务器
 async fn create_connect_channel() -> io::Result<TcpStream> {
     let tcp_listener = TcpListener::bind("0.0.0.0:5541").await?;
-    
+
     loop {
         match tcp_listener.accept().await {
-            Ok(stream) => {
-                println!("通道服务器连接成功");
+            Ok(mut stream) => {
+                println!("通道服务器({:?})连接成功", stream.1);
+
+                let msg = RTCPMessage::new(RTCPType::Initialize, BytesMut::new());
+                stream.0.write_all(msg.serialize().as_ref()).await?;
+
                 return Ok(stream.0);
             }
             Err(e) => {
                 println!("❌通道服务器连接失败{:?}", e);
-                continue
-            },
+                continue;
+            }
         };
     }
 }
@@ -30,28 +38,100 @@ async fn create_connect_channel() -> io::Result<TcpStream> {
 async fn main() -> io::Result<()> {
     let client_tcp = create_connect_channel().await?;
 
-    let client_tcp = Arc::new(Mutex::new(client_tcp));
+    let mut client_tcp = Arc::new(Mutex::new(client_tcp));
 
-    let addr = "0.0.0.0:9931";
-    let tcp_listener = TcpListener::bind(addr).await?;
-    loop {
-        let (mut tcp_stream, socket_addr) = tcp_listener.accept().await?;
-        tokio::spawn(async move {
-            let ip = socket_addr.ip().to_string();
-            println!("{ip}");
-            let mut buf = BytesMut::with_capacity(4 * 1024);
+    let client_tcp_clone = client_tcp.clone();
+    tokio::spawn(async move {
+        let rtcp_message = Mutex::<Option<RTCPMessage>>::new(None);
+        let mut buf = Arc::new(Mutex::new(BytesMut::new()));
 
-            let read_res = tcp_stream.read_buf(&mut buf).await;
+        loop {
+            let mut buf_clone = buf.clone();
+            let mut buf = buf.lock().await.as_mut();
+
+            let read_res = client_tcp_clone.lock().await.read_buf(&mut buf).await;
+
             if read_res.is_err() {
-                eprintln!("读取错误{:?}", read_res);
-                return;
+                println!("❌通道服务器读取失败{:?}", read_res);
+                break;
             }
+
             let read_res = read_res.unwrap();
 
-            // let mut http_transformer = HttpTransformer::new(tcp_stream);
-            // let _ = http_transformer.run().await;
-        });
-    }
+            let  buf: tokio::sync::MutexGuard<'_, BytesMut> = buf_clone.lock().await;
+
+            if rtcp_message.lock().await.is_none() {
+                match RTCPMessage::deserialize(buf) {
+                    Ok(res) => {
+                        let mut rtcp_message_mut = rtcp_message.lock().await;
+                        *rtcp_message_mut = Some(res);
+                    }
+                    Err(e) => {
+                        println!("序列化失败,继续读取{:?}", e);
+                        continue;
+                    }
+                };
+            }
+
+            let mut rtcp_message_mutex = rtcp_message.lock().await;
+
+            /// 这里其实可以不需要判断
+            if rtcp_message_mutex.is_none() {
+                continue;
+            }
+
+            let rtcp_message = rtcp_message_mutex.as_mut().unwrap();
+            match rtcp_message.message_type {
+                // client 、 server  都需要实现
+                RTCPType::Transformation(data_len) => {
+                    if rtcp_message.data.len() < data_len {
+                        continue;
+                    }
+                    // let mut buf = BytesMut::with_capacity(data_len);
+                    // 获取 date_len 的数据给 用户 client tcp
+                    //
+                    todo!();
+                    // 发送完毕之后吃掉发送的长度，然后再继续读取
+                    rtcp_message.data.advance(data_len);
+                    *rtcp_message_mutex = None;
+                    // 读取下一条数据
+                    continue;
+                }
+                // 其他 arm 需要 client 实现
+                _ => todo!(),
+            }
+
+            // 关闭连接
+            if read_res == 0 {
+                println!("✅通道服务器读取结束");
+
+                break;
+            }
+        }
+    });
+
+    Ok(())
+
+    // let addr = "0.0.0.0:9931";
+    // let tcp_listener = TcpListener::bind(addr).await?;
+    // loop {
+    //     let (mut tcp_stream, socket_addr) = tcp_listener.accept().await?;
+    //     tokio::spawn(async move {
+    //         let ip = socket_addr.ip().to_string();
+    //         println!("{ip}");
+    //         let mut buf = BytesMut::with_capacity(4 * 1024);
+
+    //         let read_res = tcp_stream.read_buf(&mut buf).await;
+    //         if read_res.is_err() {
+    //             eprintln!("读取错误{:?}", read_res);
+    //             return;
+    //         }
+    //         let read_res = read_res.unwrap();
+
+    //         // let mut http_transformer = HttpTransformer::new(tcp_stream);
+    //         // let _ = http_transformer.run().await;
+    //     });
+    // }
 }
 
 struct HttpTransformer {
